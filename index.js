f32f7de36a035cc14fac7c655acb494cd8c89366
Joslyn <joslynsipod@gmail.com>
2022-01-15 16:50:10 -0800

    commit tree f8fce454df106c90e5274a051f8f69ebcaebf591

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (parameter1, parameter2) => {
+    console.log(`divide ${parameter1}`);
+    console.log(`divide ${parameter2}`);
+    return 2000/100;
+}
+const square = (x) => {
+    return x * x ; 
+}
+const add = (parameter1, parameter2) => {
+    console.log(`add ${parameter1}`);
+    console.log(`add ${parameter2}`);
+    return 3 + 4;
+}
